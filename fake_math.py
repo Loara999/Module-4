01419d09ba3563b5dc7011da0a58e2647d2c96ce
Loara999 <54015247+Loara999@users.noreply.github.com>
2024-10-07 20:18:10 +0500

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,7 @@
+def divide(first: float, second: float):
+    if second == 0:
+        return 'Ошибка! На 0 делить нельзя!'
+    else:
+        return first / second
+
+
---
+++ true_math.py
@@ -0,0 +1,9 @@
+from math import inf
+
+
+def divide(first: float, second: float):
+    if second == 0:
+        return inf
+    else:
+        return first / second
+
